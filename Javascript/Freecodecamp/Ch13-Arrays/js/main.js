648cfe9d3f38809b8cfeca2d6c06c8b4103fd6b3
Swapnil Roy <68826225+thespcrewroy@users.noreply.github.com>
2024-05-22 19:21:43 -0400

    Finished Chapters 13 and 14

--- Javascript/Freecodecamp/Ch13-Arrays/js/main.js
+++ Javascript/Freecodecamp/Ch13-Arrays/js/main.js
@@ -1,30 +1,108 @@
-// var, let, and const are used to declare variables in JavaScript.
-var x = 1; // var was used in javascript until 2015, and is not considered legacy code.
-var x = 2; // var can be redeclared, which can get confusing as it does not throw an error.
-console.log(x); // 2
+let myArray = [];
 
-let y = 1;
-y = 2; // let can be reassigned, but not redeclared.
-console.log(x); // 2
+// add items to an array
+myArray[0] = "Dave";
+myArray[1] = 1001;
+myArray[2] = false;
 
-const z = 1;
-// z = 2; // const cannot be reassigned or redeclared. The preferred way to declare variables in modern JavaScript.
-console.log(x); // TypeError: Assignment to constant variable.
+// refer to an array
+console.log("Array: " + myArray); // ["Dave", 1001, false]
 
-// global scope
-var q = 1;
+// length property
+console.log("Array Length: " + myArray.length); // 3
 
-// local scope
-{
-    let q = 4;
-    console.log(q); // will execute 4
-}
+// last item in an array
+console.log("Last item: " + myArray[myArray.length - 1]); // false
 
-// local scope
-function myFunc() {
-    let q = 5;
-    console.log(q); // will execute 5
-}
+// first item in an array
+console.log("First item: " + myArray[0]); // Dave
 
-myFunc(); // will execute
-console.log(q); // will execute 1
+// add items to an array
+myArray.push("school");
+console.log("Add new item at the end: " + myArray); // ["Dave", 1001, false, "school"]
+
+// remove the last item from an array
+const lastItem = myArray.pop();
+console.log("Remove last item: " + myArray); // ["Dave", 1001, false]
+console.log("last item: " + lastItem); // school
+
+// add item to the beginning of an array
+const newLength = myArray.unshift(42);
+console.log("Add new item at the start: " + myArray); // [42, "Dave", 1001, false]
+console.log("New length: " + newLength); // 4
+
+// remove the first item from an array
+const firstItem = myArray.shift();
+console.log("Remove item from beigining: " + myArray); // ["Dave", 1001, false]
+console.log("First item: " + firstItem); // 42
+
+// delete an item from an array
+delete myArray[1];
+console.log("Delete item 1: " + myArray); // ["Dave", empty, false]
+console.log("Item 1: " + myArray[1]); // undefined
+
+// splice method - better than delete
+myArray.splice(1, 1);
+console.log("Delete item 1: " + myArray); // ["Dave", false]
+console.log("New item 1: " + myArray[1]); // false
+
+// inset an item within an array
+myArray.splice(1, 0, 42);
+console.log("Insert 42 into item 1: " + myArray); // ["Dave", 42, false]
+
+// replace an item within an array
+myArray.splice(1, 1, "Dave Jr.");
+console.log("Replace 42 with Dave Jr.: " + myArray); // ["Dave", "Dave Jr.", false]
+
+myArray = ["A", "B", "C", "D", "E", "F"];
+console.log("New Array: " + myArray); // New Array: A,B,C,D,E,F
+
+// slice method
+const newArray = myArray.slice(2, 5);
+console.log("Sliced Array: " + newArray); // Sliced Array: C,D,E
+
+// reverse method
+myArray.reverse();
+console.log("Reversed Array: " + myArray); // Reversed Array: F,E,D,C,B,A
+
+// array to string
+const newString = myArray.join();
+console.log("Array To String: " + newString); // Joined Array: F,E,D,C,B,A
+
+// string to array
+const newArray2 = newString.split(",");
+console.log("String To Array: " + newArray2); // Split Array: F,E,D,C,B,A
+
+const myArrayA = ["A", "B", "C"];
+const myArrayB = ["D", "E", "F"];
+
+// concat method
+const newArray3 = myArrayA.concat(myArrayB);
+console.log("Concatenated Array: " + newArray3); // Concatenated Array: A,B,C,D,E,F
+
+// spread operator - take the values in the array and spread them out to the new array
+const newArray4 = [...myArrayA, ...myArrayB];
+console.log("Spread Operator: " + newArray4); // Spread Operator: A,B,C,D,E,F
+
+// no spread operator
+const newArray5 = [myArrayA, myArrayB];
+console.log("No Spread Operator: " + newArray5); // No Spread Operator: A,B,C,D,E,F
+
+/* Two Dimensional Array */
+const equipShelfA = ["baseball", "football", "volleyball"];
+const equipShelfB = ["basketball", "golf balls", "tennis balls"];
+const clothesShelfA = ["tank tops", "t-shirts", "jerseys"];
+const clothesShelfB = ["sweat tops", "sweat pants", "hoodies"];
+
+console.log(equipShelfA[1]); // football
+console.log(clothesShelfB[0]) // sweat tops
+
+const equipDept = [equipShelfA, equipShelfB];
+const clothesDept = [clothesShelfA, clothesShelfB];
+
+console.log(equipDept[0][1]); // football
+console.log(clothesDept[1][0]); // sweat tops
+
+const sportsStore = [equipDept, clothesDept];
+console.log(sportsStore[0][0][1]); // football
+console.log(sportsStore[1][1][2]); // hoodies
---
+++ Javascript/Freecodecamp/Ch14-Objects/js/main.js
@@ -0,0 +1,67 @@
+// key-value pairs in curly braces
+const myObj = { name: "Dave" };
+
+/* Object within objects. Functions within objects. Accessing object properties */
+const anotherObj = {
+    alive: true,
+    answer: 42,
+    hobbies: ["Eat", "Sleep", "Code"],
+    beverage: {
+        morning: "Coffee",
+        afternoon: "Iced Tea"
+    },
+    action: function() {
+        return `Time for ${this.beverage.morning}`;
+    }
+};
+console.log(anotherObj.alive); // true
+console.log(anotherObj.hobbies[0]); // Eat
+console.log(anotherObj.action()); // Time for Coffee
+
+
+/* Inheritance */
+const vehicle = {
+    wheels: 4,
+    engine: function () {
+        return "Vrroooom!";
+    }
+};
+
+const truck = Object.create(vehicle);
+truck.doors = 2; // adding a new property
+console.log(truck); // {doors: 2}
+console.log(truck.wheels); // 4
+console.log(truck.engine()); // Vrroooom!
+
+const car = Object.create(vehicle);
+car.doors = 4; // adding a new property
+car.engine = function() { return "Whoosh!" }; // overriding the engine method
+console.log(car.engine()); // Whoosh!
+
+const tesla = Object.create(car);
+console.log(tesla.wheels); // 4
+tesla.engine = function() { return "Shhhh..." }; // overriding the engine method
+console.log(tesla.engine()); // Shhhh...
+
+/* Object.keys(), Object.values(), for...in loop */
+const band = {
+    vocals: "Robert Plant",
+    guitar: "Jimmy Page",
+    bass: "John Paul Jones",
+    drums: "John Bonham"
+};
+console.log(Object.keys(band)); // ["vocals", "guitar", "bass", "drums"]
+console.log(Object.values(band)); // ["Robert Plant", "Jimmy Page", "John Paul Jones", "John Bonham"]
+
+for (let job in band) {
+    console.log(`On ${job}, it's ${band[job]}`);
+}
+
+/* Deconstructing objects */
+const { vocals, guitar, bass, drums } = band;
+console.log(guitar);
+console.log(vocals);
+
+/* Creating a function */
+function sings({ vocals }) { return `${vocals} sings` };
+console.log(sings(band));

83f5f4ea8ed08bd659aa19f492651ee49c177600
Swapnil Roy <68826225+thespcrewroy@users.noreply.github.com>
2024-05-22 21:05:10 -0400

    Finished JSON chapter

--- Javascript/DaveGray/Ch16-JSON/js/main.js
+++ Javascript/DaveGray/Ch16-JSON/js/main.js
@@ -5,3 +5,23 @@ JSON is a text format that is completely language independent.
 Meaning JSON is used to send and receive data in many languages
 */
 
+const myObj = {
+    name: "Dave",
+    hobbies: ["Eat", "Sleep", "Code"],
+    hello: function() {
+        console.log("Hello!");
+    }
+};
+console.log(myObj);
+console.log(myObj.name);
+myObj.hello();
+console.log(typeof myObj);
+
+const sendJSON = JSON.stringify(myObj); // Convert object to JSON
+console.log(sendJSON);
+console.log(typeof sendJSON); // string
+console.log(sendJSON.name); // undefined - you can't access object properties in a string
+
+const receiveJSON = JSON.parse(sendJSON); // Convert JSON to object
+console.log(receiveJSON); // we loose the function in the object when we convert it to JSON and back
+console.log(typeof receiveJSON); // object

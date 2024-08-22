7fe35953cd42ff4d4ce46d08f6609442bf88bc3a
Swapnil Roy <68826225+thespcrewroy@users.noreply.github.com>
2024-08-22 13:23:13 -0400

    Delete MobileDevelopment/Flutter/Projects/QuickNotes directory

--- MobileDevelopment/Flutter/Projects/QuickNotes/backend/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer # allows us to convert our model (database) to JSON
-from .models import Note
-
-class NoteSerializer(ModelSerializer): # inherits from ModelSerializer
-    class Meta:
-        model = Note # specify the model we want to serialize
-        fields = '__all__' # specify the fields we want to serialize
